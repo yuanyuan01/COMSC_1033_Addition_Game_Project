933611793575dff2a1915042fb47f914bfa8db23
Jeremy Evert <jeremy.evert@swosu.edu>
2015-10-09 08:30:52 -0500

    add outline to master1412

--- src/COMSC_1033_Addition_Game_Class.java
+++ src/COMSC_1033_Addition_Game_Class.java
@@ -2,6 +2,27 @@ public class COMSC_1033_Addition_Game_Class {
 	public static void main(String[] args) {
 		System.out.println("Hello Class!");
 		// This is my master 1412 branch.
+		
+		// Outline for addition problem.
+		
+		// Round 1
+		//	Generate 2 random numbers
+		//  Ask the user to add these two numbers together
+		//  Read in their response
+		//  Check if the answer was correct
+		//		IF correct
+		//			Tell them it was correct
+		//			Give them points
+		//			Make the next question harder
+		//		IF not correct
+		//			Tell them it was wrong
+		// 			Tell them the correct answer
+		//			Do not give them points
+		//			Make the next question easier
+		// Round 2
+		// Round 3
+		// Round 4
+
 
 	}
 }
