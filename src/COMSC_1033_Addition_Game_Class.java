bee00c6afe5581b739576d60c71490c07eb09a01
Jeremy Evert <jeremy.evert@swosu.edu>
2015-10-09 09:53:55 -0500

    Started writing code

--- src/COMSC_1033_Addition_Game_Class.java
+++ src/COMSC_1033_Addition_Game_Class.java
@@ -1,15 +1,36 @@
+import java.util.Scanner;
 public class COMSC_1033_Addition_Game_Class {
 	public static void main(String[] args) {
 		System.out.println("Hello Class!");
-		// This is my master branch for 1411
+		// This is my development branch for 1411
 		// Outline for addition problem.
 		
 		// Round 1
 		//	Generate 2 random numbers
+		int number1 = (int)(Math.random() *10);
+		System.out.println("Number1 is :" + number1 );
+		
+		int number2 = (int)(Math.random() *10);
+		System.out.println("Number2 is :" + number2 );
+		
+		int correctAnswer = number1 + number2;
+		
 		//  Ask the user to add these two numbers together
+		System.out.println
+		("What is " + number1 + " + " + number2 + "?");
+		
 		//  Read in their response
+		Scanner input = new Scanner(System.in);
+		int studentAnswer = input.nextInt();
 		//  Check if the answer was correct
 		//		IF correct
+		if (studentAnswer == correctAnswer){
+			System.out.println("Answer was correct");
+		}else{
+			System.out.println("Answer was not correct");
+		}
+			
+		
 		//			Tell them it was correct
 		//			Give them points
 		//			Make the next question harder
