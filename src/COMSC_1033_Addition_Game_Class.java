7df0a09ddfc94042387613e9cfadcc14297afc51
Jeremy Evert <jeremy.evert@swosu.edu>
2015-10-09 10:34:26 -0500

    Completed outline in dev branch

--- src/COMSC_1033_Addition_Game_Class.java
+++ src/COMSC_1033_Addition_Game_Class.java
@@ -1,7 +1,7 @@
 public class COMSC_1033_Addition_Game_Class {
 	public static void main(String[] args) {
 		System.out.println("Hello Class!");
-		// This is my master 1412 branch.
+		// This is my development 1412 branch.
 		
 		// Outline for addition problem.
 		
@@ -20,8 +20,47 @@ public class COMSC_1033_Addition_Game_Class {
 		//			Do not give them points
 		//			Make the next question easier
 		// Round 2
+		//	Generate 2 random numbers
+		//  Ask the user to add these two numbers together
+		//  Read in their response
+		//  Check if the answer was correct
+		//		IF correct
+		//			Tell them it was correct
+		//			Give them points
+		//			Make the next question harder
+		//		IF not correct
+		//			Tell them it was wrong
+		// 			Tell them the correct answer
+		//			Do not give them points
+		//			Make the next question easier
 		// Round 3
+		//	Generate 2 random numbers
+		//  Ask the user to add these two numbers together
+		//  Read in their response
+		//  Check if the answer was correct
+		//		IF correct
+		//			Tell them it was correct
+		//			Give them points
+		//			Make the next question harder
+		//		IF not correct
+		//			Tell them it was wrong
+		// 			Tell them the correct answer
+		//			Do not give them points
+		//			Make the next question easier
 		// Round 4
+		//	Generate 2 random numbers
+		//  Ask the user to add these two numbers together
+		//  Read in their response
+		//  Check if the answer was correct
+		//		IF correct
+		//			Tell them it was correct
+		//			Give them points
+		//			Make the next question harder
+		//		IF not correct
+		//			Tell them it was wrong
+		// 			Tell them the correct answer
+		//			Do not give them points
+		//			Make the next question easier
 
 
 	}
